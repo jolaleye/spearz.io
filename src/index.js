f078452404b4a8a14cbf9b3b29c6926e8cab42e5
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-05-14 20:41:32 -0500

    Add initial socket connection logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
 
 import './main.css';
 import StartContainer from './components/Start/StartContainer';
@@ -7,15 +8,35 @@ import Game from './components/Game';
 import Restart from './components/Restart';
 
 class App extends Component {
-  state = { view: 'start' }
+  state = {
+    socket: null,
+    room: null,
+    view: 'start',
+  };
+
+  async componentDidMount() {
+    // connect to the server
+    await this.setState({ socket: io.connect() });
+    const { socket } = this.state;
+
+    socket.on('roomId', id => this.setState({ room: id }));
+    socket.on('rooms', rooms => console.log(rooms));
+  }
 
   changeView = view => this.setState({ view });
 
   render = () => {
     const { view } = this.state;
 
-    if (view === 'start') return <StartContainer changeView={this.changeView} />;
-    else if (view === 'game') return <Game changeView={this.changeView} />;
+    if (view === 'start') {
+      return (
+        <StartContainer socket={this.state.socket}
+          changeView={this.changeView} room={this.state.room}
+        />
+      );
+    } else if (view === 'game') {
+      return <Game changeView={this.changeView} />;
+    }
     return <Restart changeView={this.changeView} />;
   };
 }
