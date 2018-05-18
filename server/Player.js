ed66f4ab4ea17b2156bc95bd4ec25a671942e0e4
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-05-17 20:34:42 -0500

    Add initial position

--- server/Player.js
+++ server/Player.js
@@ -1,8 +1,20 @@
+const _ = require('lodash');
+
+const config = require('./config');
+
 class Player {
   constructor(id, name, room) {
     this.id = id;
     this.name = name;
     this.room = room;
+    // random initial position within the arena (a circle)
+    // origin is at the center of the arena
+    const randomAngle = _.random(2 * Math.PI);
+    const randomDistance = _.random(config.arenaRadius ** 2);
+    this.pos = {
+      x: Math.sqrt(randomDistance) * Math.cos(randomAngle),
+      y: Math.sqrt(randomDistance) * Math.sin(randomAngle),
+    };
   }
 }
 
