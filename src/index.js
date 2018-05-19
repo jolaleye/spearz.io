e71ae1f8fae77a9a5a9a8739e805b2463cd07773
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-05-19 13:29:58 -0500

    Load assets when the app mounts

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import './main.css';
 import StartContainer from './components/Start/StartContainer';
 import GameContainer from './components/Game/GameContainer';
 import Restart from './components/Restart';
+import arenaBg from './assets/arena-bg.png';
+
+const { LoadQueue } = window.createjs;
 
 class App extends Component {
   state = {
     socket: null,
     room: null,
     view: 'start',
+    queue: null,
+    assets: {},
+    allLoaded: false,
   };
 
   async componentDidMount() {
@@ -19,19 +25,46 @@ class App extends Component {
     await this.setState({ socket: io.connect() });
     const { socket } = this.state;
 
+    // load game assets
+    this.loadAssets();
+
+    // receiving the current room id
     socket.on('roomId', id => this.setState({ room: id }));
+
+    // name submitted, ready to play
     socket.on('ready', () => this.changeView('game'));
+  }
+
+  loadAssets = async () => {
+    await this.setState({ queue: new LoadQueue(false) });
+    const { queue } = this.state;
+
+    const handleFileLoad = e => {
+      // add each asset to state
+      this.setState(prevState => ({
+        assets: { ...prevState.assets, [e.item.id]: e.result },
+      }));
+    };
+    queue.on('fileload', handleFileLoad, this);
+    queue.on('complete', () => this.setState({ allLoaded: true }));
+
+    // list of assets to be loaded
+    queue.loadManifest([
+      { id: 'background', src: arenaBg },
+    ]);
   }
 
   changeView = view => this.setState({ view });
 
   render = () => {
-    const { socket, view, room } = this.state;
+    const { socket, view, room, allLoaded, assets } = this.state;
 
     if (view === 'start') {
       return <StartContainer socket={socket} room={room} />;
     } else if (view === 'game') {
-      return <GameContainer socket={socket} />;
+      return allLoaded
+        ? <GameContainer socket={socket} assets={assets} />
+        : <div>loading</div>;
     }
     return <Restart />;
   }
