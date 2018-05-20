343a86f4e4ab4dce04fe81947adb104951339ff4
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-05-20 12:27:50 -0500

    Modify health and shield bar appearance

--- src/components/Game/Status/Status.js
+++ src/components/Game/Status/Status.js
@@ -4,8 +4,8 @@ import './Status.css';
 
 const Status = ({ health, shield }) => (
   <div className="status">
-    <progress className="status__bar progress is-primary" value={shield} max="100" />
-    <progress className="status__bar progress is-danger" value={health} max="100" />
+    <progress className="status__bar status__shield progress is-small" value={shield} max="100" />
+    <progress className="status__bar status__health progress is-small" value={health} max="100" />
   </div>
 );
 
