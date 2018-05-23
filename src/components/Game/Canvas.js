bbf238cda85d8898a2cf9745175d2938847c6ac4
Jonathan <j.olaleye.o@gmail.com>
2018-05-22 19:43:43 -0500

    Add player drawing, rework background drawing

--- src/components/Game/Canvas.js
+++ src/components/Game/Canvas.js
@@ -1,20 +1,27 @@
-import React, { Component, createRef } from 'react';
+import React, { Component, createRef, Fragment } from 'react';
 
 import config from '../../config';
+import playerSprite from '../../assets/player.png';
+import spearSprite from '../../assets/spear.png';
 
-const { Stage, Bitmap } = window.createjs;
+const { Stage, Bitmap, Container } = window.createjs;
 
 class Canvas extends Component {
   canvas = createRef();
   backgroundCell = this.props.assets.backgroundCell;
+  playerImg = new Bitmap(playerSprite);
+  spearImg = new Bitmap(spearSprite);
 
   state = {
     pos: { x: 0, y: 0 },
+    direction: 90,
   }
 
   componentDidMount() {
     // Easel JS
     this.stage = new Stage(this.canvas.current);
+    this.playerContainer = new Container();
+    this.playerContainer.addChild(this.playerImg);
 
     // initialize the canvas
     this.resizeCanvas();
@@ -40,13 +47,37 @@ class Canvas extends Component {
     };
 
     this.props.socket.emit('requestUpdate', mouse, data => {
-      this.setState({ pos: data.player.pos });
-      this.drawBackground(data.player.pos);
+      this.setState({
+        pos: data.player.pos,
+        direction: data.player.direction,
+      });
     });
+
+    this.drawBackground();
+    this.drawPlayer();
+    this.stage.update();
   }
 
-  drawBackground = pos => {
+  drawPlayer = () => {
+    const { stage, playerContainer } = this;
+
+    stage.addChild(playerContainer);
+
+    // set player container's registration point to its center
+    playerContainer.regX = playerContainer.getBounds().width / 2;
+    playerContainer.regY = playerContainer.getBounds().height / 2;
+
+    // center the player
+    playerContainer.x = stage.canvas.width / 2;
+    playerContainer.y = stage.canvas.height / 2;
+
+    // rotate the player to the direction it's facing in game
+    playerContainer.rotation = this.state.direction + 90;
+  }
+
+  drawBackground = () => {
     const { stage, backgroundCell } = this;
+    const { pos } = this.state;
 
     const xNumOfCells = Math.ceil(stage.canvas.width / backgroundCell.width) + 1;
     const yNumOfCells = Math.ceil(stage.canvas.height / backgroundCell.height) + 1;
@@ -54,21 +85,20 @@ class Canvas extends Component {
     const xOffset = pos.x % backgroundCell.width;
     const yOffset = pos.y % backgroundCell.height;
 
-    stage.setTransform(-xOffset, -yOffset);
-
+    // draw cells around the player
     for (let x = -xNumOfCells; x < xNumOfCells; x += 1) {
       for (let y = -yNumOfCells; y < yNumOfCells; y += 1) {
         const bgCell = new Bitmap(backgroundCell);
-        bgCell.setTransform(x * bgCell.getBounds().width, y * bgCell.getBounds().height);
+        bgCell.setTransform(
+          -xOffset + (x * bgCell.getBounds().width),
+          -yOffset + (y * bgCell.getBounds().height),
+        );
         stage.addChild(bgCell);
-        stage.update();
       }
     }
-
-    stage.setTransform();
   }
 
-  render = () => <canvas ref={this.canvas} />
+  render = () => <canvas ref={this.canvas} />;
 }
 
 export default Canvas;
