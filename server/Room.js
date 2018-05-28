9ec0de9bddde2ba4abda443ebc6ddc24c923f0e5
Jonathan <j.olaleye.o@gmail.com>
2018-05-27 22:34:53 -0500

    Add function to fetch other players

--- server/Room.js
+++ server/Room.js
@@ -1,9 +1,24 @@
-const { id } = require('./util');
+const { id, getDistance } = require('./util');
 
 class Room {
   constructor() {
     this.id = id();
     this.players = [];
+  }
+
+  fetchOtherPlayers(currentPlayer) {
+    // exclude the current player
+    const otherPlayers = this.players.filter(player => player.id !== currentPlayer.id);
+    // only include players within 2500 units of the current player
+    const nearbyPlayers = otherPlayers.filter(player => {
+      const distance = getDistance(
+        currentPlayer.pos.x, player.pos.x,
+        currentPlayer.pos.y, player.pos.y,
+      );
+      return distance.total <= 2500;
+    });
+
+    return nearbyPlayers;
   }
 }
 
