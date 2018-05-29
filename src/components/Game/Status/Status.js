774955435fbeb5a4cc1775be8a7e77e81aca3237
Jonathan <j.olaleye.o@gmail.com>
2018-05-28 20:01:47 -0500

    Remove shield bar

--- src/components/Game/Status/Status.js
+++ src/components/Game/Status/Status.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 import './Status.css';
 
-const Status = ({ health, shield }) => (
+const Status = ({ health }) => (
   <div className="status">
-    <progress className="status__bar status__shield progress is-small" value={shield} max="100" />
     <progress className="status__bar status__health progress is-small" value={health} max="100" />
   </div>
 );
--- src/components/Game/Status/StatusContainer.js
+++ src/components/Game/Status/StatusContainer.js
@@ -5,19 +5,21 @@ import Status from './Status';
 class StatusContainer extends Component {
   state = {
     health: 0,
-    shield: 0,
   }
 
   componentDidMount() {
     this.props.socket.on('status', status => {
       this.setState({
         health: status.health,
-        shield: status.shield,
       });
     });
   }
 
-  render = () => <Status health={this.state.health} shield={this.state.shield} />;
+  componentWillUnmount() {
+    this.props.socket.off('status');
+  }
+
+  render = () => <Status health={this.state.health} />;
 }
 
 export default StatusContainer;
