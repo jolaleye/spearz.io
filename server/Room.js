99af8e9dce2fe1c130eb0891abf017cad1fb5252
Jonathan <j.olaleye.o@gmail.com>
2018-05-31 18:58:39 -0500

    Add methods to fetch other players and check spear hits

--- server/Room.js
+++ server/Room.js
@@ -1,3 +1,5 @@
+const { testPolygonPolygon } = require('sat');
+
 const { id, getDistance } = require('./util');
 
 class Room {
@@ -15,6 +17,34 @@ class Room {
         currentPlayer.pos.y, player.pos.y,
       );
       return distance.total <= 2500;
+    });
+  }
+
+  fetchOtherPlayers(currentPlayer) {
+    // only include players within 2500 units of the current player
+    // (not including the current player)
+    return this.players.filter(player => {
+      const distance = getDistance(
+        currentPlayer.pos.x, player.pos.x,
+        currentPlayer.pos.y, player.pos.y,
+      );
+      return (distance.total <= 2500) && (player.id !== currentPlayer.id);
+    });
+  }
+
+  checkForHits(currentPlayer) {
+    const players = this.fetchOtherPlayers(currentPlayer);
+    players.forEach(otherPlayer => {
+      // test for collision between the spear and a player
+      if (testPolygonPolygon(currentPlayer.spear.hitbox, otherPlayer.hitbox)) {
+        currentPlayer.resetSpear();
+        otherPlayer.takeHit(25);
+
+        // if the player is now dead, remove them from the room
+        if (!otherPlayer.checkStatus()) {
+          this.players = this.players.filter(player => player.id !== otherPlayer.id);
+        }
+      }
     });
   }
 }
