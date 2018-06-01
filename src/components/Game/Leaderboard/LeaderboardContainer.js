d783b215ce9f0de9d2bde2ebe723e1021a47c96a
Jonathan <j.olaleye.o@gmail.com>
2018-05-31 21:05:31 -0500

    Implement real leaderboard

--- src/components/Game/Leaderboard/LeaderboardContainer.js
+++ src/components/Game/Leaderboard/LeaderboardContainer.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 import Leaderboard from './Leaderboard';
 
 class LeaderboardContainer extends Component {
-  render = () => <Leaderboard />;
+  state = {
+    leaderboard: [],
+  }
+
+  componentDidMount() {
+    this.props.socket.on('leaderboard', leaderboard => {
+      if (!_.isEqual(leaderboard, this.state.leaderboard)) this.setState({ leaderboard });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.socket.off('leaderboard');
+  }
+
+  render = () => <Leaderboard leaders={this.state.leaderboard} />;
 }
 
 export default LeaderboardContainer;
