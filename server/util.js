65e8eac6c64133016cabe2931047d8fb216c276b
Jonathan <j.olaleye.o@gmail.com>
2018-06-01 20:46:55 -0500

    Rename id to createId

--- server/util.js
+++ server/util.js
@@ -1,4 +1,4 @@
-exports.id = () => Math.random().toString(36).substr(2, 9);
+exports.createId = () => Math.random().toString(36).substr(2, 9);
 
 exports.getDistance = (x1, x2, y1, y2) => ({
   x: x2 - x1,
