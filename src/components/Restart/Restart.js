d47d81dc76c06fe8141374f52927c69b16358924
Jonathan <j.olaleye.o@gmail.com>
2018-06-03 14:51:38 -0500

    Add killed by to restart screen

--- src/components/Restart/Restart.js
+++ src/components/Restart/Restart.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import './Restart.css';
 
-const Restart = ({ changeView }) => (
+const Restart = ({ changeView, killedBy }) => (
   <div className="restart">
+    <span className="restart__killed-by">Killed by <span>{killedBy}</span></span>
     <button className="form__btn button is-large is-rounded" onClick={() => changeView('start')}>
       <span className="icon"><i className="fas fa-arrow-right"></i></span>
     </button>
