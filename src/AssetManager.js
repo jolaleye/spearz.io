0f17f8a4643a762a06177610e728ff96486e8c22
Jonathan <j.olaleye.o@gmail.com>
2018-06-03 14:05:58 -0500

    Add AssetManager to control loading and access to assets

---
+++ src/AssetManager.js
@@ -0,0 +1,45 @@
+import { Howl } from 'howler';
+
+import background from './assets/bg-cell.png';
+import player from './assets/player.png';
+import spear from './assets/spear.png';
+import spearReleased from './assets/spear-released.png';
+import heartbeat from './assets/heartbeat.wav';
+import hit from './assets/hit.wav';
+import soundtrack from './assets/soundtrack.mp3';
+import throwSFX from './assets/throw.wav';
+
+const { LoadQueue } = window.createjs;
+
+class AssetManager {
+  constructor() {
+    this.queue = new LoadQueue(false);
+    this.queue.on('fileload', this.handleLoad, this);
+
+    this.assets = {};
+    this.sprites = { player, spear, spearReleased };
+    this.sounds = {
+      heartbeat: new Howl({ src: heartbeat, loop: true, volume: 0.6 }),
+      hit: new Howl({ src: hit, volume: 0.2 }),
+      soundtrack: new Howl({ src: soundtrack, loop: true, volume: 0.2 }),
+      throw: new Howl({ src: throwSFX, volume: 0.2 }),
+    };
+  }
+
+  loadAssets = () => {
+    this.queue.loadManifest([
+      { id: 'background', src: background },
+    ]);
+
+    return new Promise((resolve, reject) => {
+      this.queue.on('complete', resolve, this);
+      this.queue.on('error', reject, this);
+    });
+  }
+
+  handleLoad = e => {
+    this.assets[e.item.id] = e.result;
+  }
+}
+
+export default new AssetManager();
