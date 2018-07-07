eb99e8c1e3e251fe7999efd085403249a98b7c8f
Jonathan <j.olaleye.o@gmail.com>
2018-07-07 14:48:49 -0500

    Set up Pixi app and canvas

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
+import { Application } from 'pixi.js';
 
 class Game extends Component {
-  render = () => <div>Game</div>
+  canvasRef = createRef();
+
+  componentDidMount() {
+    // PIXI application
+    this.app = new Application({
+      view: this.canvasRef.current,
+      antialias: true,
+      resolution: window.devicePixelRatio,
+      transparent: true,
+    });
+
+    // canvas sizing
+    this.app.renderer.autoResize = true;
+    window.addEventListener('resize', this.resize);
+    this.resize();
+  }
+
+  resize = () => {
+    this.app.renderer.resize(window.innerWidth, window.innerHeight);
+  }
+
+  render = () => (
+    <div className="game">
+      <canvas ref={this.canvasRef} />
+    </div>
+  );
 }
 
 export default Game;
