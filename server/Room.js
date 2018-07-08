49e8f37e1bdaa4486c03ab26b04f3170ef1c747d
Jonathan <j.olaleye.o@gmail.com>
2018-07-08 11:33:24 -0500

    addClient & getNearbyPlayers methods | snapshot tick
    - addClient sends the client the room key and increases connection count
    - in the simulate tick, give each client a property
    with their last acknowledged tick
    - add snapshot tick
    - getNearbyPlayers finds players within a given distance

--- server/Room.js
+++ server/Room.js
@@ -1,21 +1,32 @@
 const _ = require('lodash');
 
 const config = require('./config');
-const { ID } = require('./services/util');
+const { ID, getDistance } = require('./services/util');
+const { pack } = require('./services/cereal');
 
 class Room {
   constructor() {
     this.key = ID();
     this.connections = 0;
     this.clients = {};
+    this.players = [];
     this.queue = [];
 
     // simulation tick
     setInterval(this.simulate.bind(this), config.tickrate);
+    // snapshot tick
+    setInterval(this.snapshot.bind(this), config.snapshotRate);
+  }
+
+  addClient(client) {
+    client.room = this.key;
+    this.connections += 1;
+    client.send(pack({ _: 'roomKey', key: this.key }));
   }
 
   removeClient(id) {
     this.clients = _.omitBy(this.clients, client => client.id === id);
+    this.players = this.players.filter(player => player.id !== id);
     this.connections -= 1;
   }
 
@@ -24,6 +35,14 @@ class Room {
   }
 
   simulate() {
+    if (_.isEmpty(this.queue)) return;
+
+    // find each client's last command
+    Object.values(this.clients).forEach(client => {
+      const last = _.findLastIndex(this.queue, command => command.clientID === client.id);
+      client.last = this.queue[last].tick;
+    });
+
     this.queue.forEach((command, i) => {
       const client = this.clients[command.clientID];
 
@@ -38,6 +57,30 @@ class Room {
       this.queue.splice(i, 1);
     });
   }
+
+  snapshot() {
+    Object.values(this.clients).forEach(client => {
+      client.send(pack({
+        _: 'snapshot',
+        timestamp: Date.now(),
+        last: client.last,
+        players: this.getNearbyPlayers(client),
+      }));
+    });
+  }
+
+  getNearbyPlayers(client) {
+    return this.players.filter(player => {
+      const distance = getDistance(
+        client.player.pos.x, player.pos.x,
+        client.player.pos.y, player.pos.y,
+      );
+
+      return distance.total <= 1250;
+    });
+  }
 }
 
 module.exports = Room;
+
+/* eslint no-param-reassign: off */
