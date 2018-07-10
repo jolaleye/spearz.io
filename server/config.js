f484a2b5a35bf3fb5042d8e3aab3dab8fcccb8bf
Jonathan <j.olaleye.o@gmail.com>
2018-07-10 15:31:29 -0500

    Add damage and score values

--- server/config.js
+++ server/config.js
@@ -1,8 +1,16 @@
 module.exports = {
   port: process.env.NODE_ENV === 'production' ? process.env.PORT : 3001,
+
   tickrate: 15,
   snapshotRate: 50,
   arenaRadius: 5000,
   playerLimit: 100,
+
   spearCooldown: 500,
+
+  boundaryDamage: 10,
+  boundaryDamageFrequency: 1000,
+  hitDamage: 20,
+
+  killScore: 100,
 };
