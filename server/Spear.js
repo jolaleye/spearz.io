3676ffbc3b13267332b8e3c0a4aba039e50bcff8
Jonathan <j.olaleye.o@gmail.com>
2018-07-10 13:57:37 -0500

    Add launch and movement logic

--- server/Spear.js
+++ server/Spear.js
@@ -6,6 +6,9 @@ class Spear {
     this.pos = { x: pos.x, y: pos.y };
     this.direction = 0;
 
+    this.vx = 0;
+    this.vy = 0;
+
     // collision bounds needed for SAT    values match the sprite
     // points ordered from the point of the spear clockwise around the spear head
     this.satPolygon = new Polygon(this.pos, [
@@ -16,7 +19,7 @@ class Spear {
   // get spear data needed on the client
   retrieve() {
     const { pos, direction } = this;
-    return { pos, direction };
+    return { pos, direction, bounds: this.bounds };
   }
 
   // data needed for the quadtree   width & height match the sprite
@@ -40,6 +43,25 @@ class Spear {
     this.pos.x = playerPos.x + (55 * Math.cos(angle));
     this.pos.y = playerPos.y + (55 * Math.sin(angle));
     this.direction = playerDirection;
+  }
+
+  launch() {
+    // launch slightly inwards towards the player
+    const launchAngle = this.local.spear.direction - (Math.PI / 26);
+    this.local.spear.direction = launchAngle;
+    this.vx = 25 * Math.cos(this.direction);
+    this.vy = 25 * Math.sin(this.direction);
+  }
+
+  move() {
+    if (!this.pos.x || !this.pos.y) return;
+
+    this.pos.x += this.vx;
+    this.pos.y += this.vy;
+
+    // gradually decrease velocity
+    this.vx *= 0.99;
+    this.vy *= 0.99;
   }
 }
 
