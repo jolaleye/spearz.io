fcf3c67eac5939a7dfe50163c10ffcdaa6bbe712
Jonathan <j.olaleye.o@gmail.com>
2018-07-11 20:19:42 -0500

    Preload message background and cursor images

--- src/assetManager.js
+++ src/assetManager.js
@@ -3,6 +3,8 @@ import * as PIXI from 'pixi.js';
 import backgroundCell from './assets/background-cell.png';
 import spriteAtlas from './assets/spritesheet.json';
 import spritesheet from './assets/spritesheet.png';
+import messageBg from './assets/message-area.svg';
+import cursor from './assets/cursor.png';
 
 class AssetManager {
   constructor() {
@@ -10,7 +12,7 @@ class AssetManager {
   }
 
   load = () => new Promise(resolve => {
-    PIXI.loader.add([backgroundCell, spritesheet]).load(() => {
+    PIXI.loader.add([backgroundCell, spritesheet, messageBg, cursor]).load(() => {
       const { resources } = PIXI.loader;
 
       this.textures.backgroundCell = resources[backgroundCell].texture;
