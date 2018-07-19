cf790d84605dc840d3eb5b318cb3667bd8c2af83
Jonathan <j.olaleye.o@gmail.com>
2018-07-19 16:07:08 -0500

    Change smiley face

--- src/components/Mobile/Mobile.js
+++ src/components/Mobile/Mobile.js
@@ -5,7 +5,7 @@ import logo from '../../assets/logo.svg';
 
 const Mobile = () => (
   <div className="mobile is-hidden-desktop">
-    <span>Please move to a larger device to play ☺</span>
+    <span>Please move to a larger device to play :)</span>
     <img src={logo} alt="spearz.io"/>
   </div>
 );
